e3bf4ba2736286845772a0ec3e997b7d21c997f4
ryangadz@msn.com <ryangadz@msn.com>
2018-03-27 14:06:36 -0700

    initial project setup

---
+++ Source/OctreePluginSample/OctreePluginSampleGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OctreePluginSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCTREEPLUGINSAMPLE_API AOctreePluginSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
